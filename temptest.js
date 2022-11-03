9089ea9560539b9d2b13e60c5fb87f1b19373246
Laetitia (O-01-67) <68935009+O-01-67@users.noreply.github.com>
2022-11-03 15:08:41 -0400

    Code fixes
    * Define tick within element object
    * Add componentToHex function
    * Use more workable values for teVs

--- temptest.js
+++ temptest.js
@@ -1,9 +1,18 @@
 // Mod testing with temperature
 
 let teVs = {
-  min: Number.MIN_VALUE,
-  max: Number.MAX_VALUE
+  min: 0,
+  max: 10000
 }
+
+function componentToHex(number) {
+	if(isNaN(number)) { number = 0 };
+	number = Math.max(0,Math.min(255,Math.round(number))).toString(16);
+	if(number.length == 1) {
+		number = "0" + number;
+	};
+	return number;
+};
 
 // https://stackoverflow.com/questions/10756313/javascript-jquery-map-a-range-of-numbers-to-another-range-of-numbers
 // Function from August Miller
@@ -22,8 +31,7 @@ elements.tmptst = {
     viscosity: 100000,
     state: "liquid",
     density: 720,
-};
-
-behaviors.tmptst.tick = function(pixel) {
-  pixel.color = rgbToHex(scale(pixel.temp, teVs.min, teVs.max, 0, 255), 0, 0)
+    tick: function(pixel) {
+        pixel.color = rgbToHex(scale(pixel.temp, teVs.min, teVs.max, 0, 255), 0, 0)
+    },
 };
